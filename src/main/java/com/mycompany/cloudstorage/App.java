ccef63f23d48f0f927617971dc313610f770fad2
Fraser <N1026954@my.ntu.ac.uk>
2023-04-17 23:16:50 +0000

    Database added to project but not yet functional

--- src/main/java/com/mycompany/cloudstorage/App.java
+++ src/main/java/com/mycompany/cloudstorage/App.java
@@ -2,23 +2,31 @@ package com.mycompany.cloudstorage;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
-
-//design colours:
-//colour: #7113ab (purple)
-//colour: #bababa (grey)
-
+import java.sql.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
     
+    Connection con = null;
+    PreparedStatement pst = null;
+    ResultSet rs = null;
     
+    @FXML
+    private Button button_login;
+    
+    @FXML
+    private TextField tf_username;
+    
+    @FXML
+    private PasswordField tf_password;
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -27,8 +35,27 @@ public class App extends Application {
         Scene scene = new Scene(loader.load());
         stage.setScene(scene);
         stage.show();
+        con = DBConnection.ConnectionDB();
     }
-    
+       
+    private void btnLoginActionPerformed(java.awt.event.ActionEvent evt) {
+        String sql = "SELECT * from user_db WHERE user LIKE ? AND Pass LIKE ?; ";
+        try{
+            pst = con.prepareStatement(sql);
+            pst.setString(1, tf_username.getText());
+            pst.setString(2, tf_password.getText());
+            rs = pst.executeQuery();
+            
+            if(rs.next()){
+                JOptionPane.showMessageDialog(null, "Login Successfull!");
+                
+            }
+        }
+        catch(Exception e){
+            
+        }
+    }
+            
 public static void main(String[] args) {
     launch(args);
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.mycompany.cloudstorage {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires java.sql;
+    requires java.desktop;
 
     opens com.mycompany.cloudstorage to javafx.fxml;
     exports com.mycompany.cloudstorage;
