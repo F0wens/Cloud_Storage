abb69ef4e7de34b5a91c9a2ff288d331dc749ec7
Fraser <N1026954@my.ntu.ac.uk>
2023-04-18 21:39:33 +0000

    Database connection successful. Login with correct details not working

--- src/main/java/com/mycompany/cloudstorage/HomepageController.java
+++ src/main/java/com/mycompany/cloudstorage/HomepageController.java
@@ -5,13 +5,43 @@
 package com.mycompany.cloudstorage;
 
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 
 public class HomepageController implements Initializable {
     
+    @FXML 
+    private Button button_log_out;
+    
+    @FXML
+    private void openLogin() {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+ 
+    @FXML
+    private void openLogOut(ActionEvent event) {
+        if (button_log_out.getId().equals("button_log_out")) {
+            openLogin();
+        }
+    }
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
